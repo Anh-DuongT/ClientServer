1cb0c1b78544b076551fccac2b47dad422da2741
DELL 5448 <DELL 5448@DELL>
2018-03-11 23:24:26 +0700

    client-server

--- ClientServer/Program.cs
+++ ClientServer/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
 
 namespace ClientServer
 {
@@ -10,6 +13,18 @@ namespace ClientServer
     {
         static void Main(string[] args)
         {
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            client.Connect(ipServer);
+            string clientSend = "Send File Text1";
+            byte[] send = Encoding.ASCII.GetBytes(clientSend);
+            client.Send(send);
+            byte[] receive = new byte[1024 * 60];
+            int rec = client.Receive(receive);
+            string clientReceive = Encoding.ASCII.GetString(receive, 0, rec);
+            StreamWriter sw = new StreamWriter("..//..//TxtClient.txt");
+            sw.Write(clientReceive);
+            sw.Close();
         }
     }
 }
---
+++ Server/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ip = new IPEndPoint(IPAddress.Loopback, 1234);
+            server.Bind(ip);
+            StreamReader sr = new StreamReader("../../TxtSever.txt");
+            string readFile = sr.ReadToEnd();
+            sr.Close();
+            server.Listen(4);
+            Socket client = server.Accept();
+            byte[] receive = new byte[1024 * 60];
+            int rec = client.Receive(receive);
+            string serverReceive = Encoding.ASCII.GetString(receive, 0, rec);
+            Console.WriteLine("Client gui: " + serverReceive);
+            byte[] send = Encoding.ASCII.GetBytes(readFile);
+            client.Send(send);
+        }
+    }
+}
